2d2d940e60c3aa95b3a177a3fa8c0feb84abe01a
Kacper Blaz <blazzkac@ul005068.eu.tieto.com>
2024-07-04 13:58:37 +0200

    import remote state, test POC pipeline

--- main.tf
+++ main.tf
@@ -21,5 +21,5 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "this" {
   name     = "test"
-  location = "West Europe"
+  location = "eastus"
 }
--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,17 +1,34 @@
-module "remote_state" {
-  source               = "./blob-storage-remote-state"
-  project_name         = var.project_name
-  environment          = var.environment
-  region               = var.region
-  resource_group_name  = var.resource_group_name
-  storage_account_name = var.storage_account_name
-  container_name       = var.container_name
-  #  subscription_id = var.subscription_id
-  #  client_id = var.client_id
-  #  client_secret = var.client_secret
-  #  tenant_id = var.tenant_id
+resource "azurerm_resource_group" "res-0" {
+  location = "eastus"
+  name     = "tfstate"
 }
-
-output "storage_name" {
-  value = module.remote_state.storage_name
+resource "azurerm_key_vault" "res-1" {
+  enable_rbac_authorization = true
+  location                  = "eastus"
+  name                      = "subsciption-id-dev"
+  resource_group_name       = "tfstate"
+  sku_name                  = "standard"
+  tags = {
+    Environment = "dev"
+  }
+  tenant_id = "f2633c0a-777e-41be-8f09-2a76fad51c13"
+  depends_on = [
+    azurerm_resource_group.res-0,
+  ]
+}
+resource "azurerm_storage_account" "res-2" {
+  account_replication_type         = "LRS"
+  account_tier                     = "Standard"
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+  location                         = "eastus"
+  name                             = "devtfstateaks"
+  resource_group_name              = "tfstate"
+  depends_on = [
+    azurerm_resource_group.res-0,
+  ]
+}
+resource "azurerm_storage_container" "res-4" {
+  name                 = "tfstate"
+  storage_account_name = "devtfstateaks"
 }
